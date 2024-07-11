d968e5ba177741b9a3b3e5e8a25ab149ba162597
Sahil <leviack629@gmail.com>
2024-07-11 23:26:32 +0530

    db connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log('hello world');
+import "dotenv/config";
+import connectDB from "./db/index.js";
+
+
+
+
+
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//first approach
+// import "dotenv/config";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("ERROR", error);
+//     throw error;
+//   }
+// })();
