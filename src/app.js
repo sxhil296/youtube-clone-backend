06631630055b7ca05720f70db9e012a650aeea34
Sahil <leviack629@gmail.com>
2024-07-12 12:18:19 +0530

    utils

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // Allowing requests only from the specified origin in the environment variable.
+    credentials: true, // Enabling credentials (cookies, authorization headers, etc.) to be included in requests.
+  })
+);
+
+// Middleware to parse incoming JSON requests with a size limit of 16KB
+app.use(express.json({ limit: "16kb" }));
+
+// Middleware to parse URL-encoded data with a size limit of 16KB
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Middleware to serve static files from the "public" directory
+app.use(express.static("public"));
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
-
-
-
-
-
-
-
-
+import { app } from "./app.js";
 
 connectDB()
+  .then(() => {
+    app.on("ERROR : ", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed!!", error);
+  });
 
 
 
@@ -24,9 +30,7 @@ connectDB()
 
 
 
-
-
-
+  
 //first approach
 // import "dotenv/config";
 // import mongoose from "mongoose";
