a6993a2aed8d5a8f4cfb5004cf466f2e2f4746e5
Sahil <leviack629@gmail.com>
2024-07-24 13:40:40 +0530

    bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //check if user already exists - username or email
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
