560ac4c8bdb6c3c562e7ec3854d993d166e05dfe
Sahil <leviack629@gmail.com>
2024-07-26 15:24:00 +0530

    login and logout

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -148,4 +148,33 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+//LOGOUT USER
+const logoutUser = asyncHandler(async (req, res) => {
+  //find user and clear refresh token
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  //clear cookies
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", refreshToken)
+    .json(new ApiResponse(200, {}, "User logged out successfully!"));
+});
+
+
+
+export { registerUser, loginUser, logoutUser };
